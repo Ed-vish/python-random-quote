0c94c54aa1ade186cb8883ac89ea8855c3e56fcc
Ed-vish <dev.youta@gmail.com>
2020-09-30 19:41:59 +0000

    play with python

--- get-quote.py
+++ get-quote.py
@@ -2,6 +2,9 @@ import random
 def primary():
    print("Keep it logically awesome.")
 
+   print "Hello World!",
+   print "My Name is Ed-Vish"
+
    f = open("quotes.txt")
    quotes = f.readlines()
    f.close()
@@ -9,6 +12,9 @@ def primary():
    last = 13
    rnd = random.randint(0, last)
    print(quotes[rnd])
+   
+   myStr = ("To leave a message\n press the # key on your phone.")
+   print(myStr)
 
 if __name__== "__main__":
   primary()
